17978cd44d2a23181413439eae51bf6238b0ae89
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-04-24 08:59:30 +0200

    Added initial project.

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mesher
+//
+//  Created by Asger Nyman Christiansen on 24/04/2017.
+//  Copyright © 2017 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
