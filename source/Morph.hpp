e6f6ac8bc4b28caf54de049c7b9bf5b64aee24a3
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-04-27 20:46:41 +0200

    Morphing an area.

--- source/Morph.hpp
+++ source/Morph.hpp
@@ -10,32 +10,58 @@
 class Morph
 {
     glm::vec3 direction;
-    mesh::Mesh* model;
-    const mesh::VertexID* vertex = nullptr;
+    mesh::Mesh *model;
+    std::shared_ptr<mesh::Attribute<mesh::VertexID, float>> weights = std::make_shared<mesh::Attribute<mesh::VertexID, float>>();
+    const float RADIUS = 1.;
+    
+    void update_weights(const glm::vec3& origin, const mesh::VertexID* vertex)
+    {
+        if(!weights->contains(vertex))
+        {
+            glm::vec3 position = model->position()->at(vertex);
+            auto dist = glm::distance(position, origin);
+            if(dist < RADIUS)
+            {
+                weights->at(vertex) = RADIUS - dist;
+                
+                for (auto face : vertex->faces())
+                {
+                    update_weights(origin, face->v1());
+                    update_weights(origin, face->v2());
+                    update_weights(origin, face->v3());
+                }
+            }
+        }
+    }
     
 public:
     
-    void start(mesh::Mesh* _model, const mesh::VertexID* _vertex)
+    void start(mesh::Mesh* _model, const mesh::VertexID* vertex)
     {
         model = _model;
-        vertex = _vertex;
         direction = model->normal(vertex);
+        update_weights(model->position()->at(vertex), vertex);
     }
     
     void update(float power)
     {
-        glm::vec3 position = model->position()->at(vertex);
-        model->position()->at(vertex) = position + direction * power * 0.01f;
+        for(auto vertex = model->vertices_begin(); vertex != model->vertices_end(); vertex = vertex->next())
+        {
+            float weight = weights->at(vertex);
+            glm::vec3 position = model->position()->at(vertex);
+            model->position()->at(vertex) = position + weight * power * 0.01f * direction;
+        }
     }
     
     void end()
     {
-        vertex = nullptr;
+        model = nullptr;
+        weights->clear();
     }
     
     bool is_morphing()
     {
-        return vertex != nullptr;
+        return weights->size() != 0;
     }
     
 };
